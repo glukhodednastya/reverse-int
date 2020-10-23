9abcf59a8de4a6f009bf96d595ac9a1b42eec662
Anastasia Glukhoded <glukhodednastya@gmail.com>
2020-10-23 16:10:08 +0300

    feat: edit index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    var num = 0;
+    while (Math.abs(n) > 0) {
+        num = num * 10 + Math.abs(n) % 10;
+        n = Math.floor(Math.abs(n) / 10);
+    }
+    return num;
 }
